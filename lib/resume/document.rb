68ebb6aa272f6f352277d8132c79d681dd51719c
Jason Kenney <therealjasonkenney@gmail.com>
2014-02-15 00:44:37 -0500

    * Cleaned up PDF code.

--- lib/resume/document.rb
+++ lib/resume/document.rb
@@ -19,9 +19,39 @@ module Resume
 
       self
     end
-    
-
-      
-  end
   
-end    
+    protected  
+
+    # Internal method to get a string with
+    # the difference in years/months between
+    # start and end dates.
+    def _time_difference(start_date, end_date)
+      t      = TimeDifference.between(
+        *([start_date,end_date].map do |d|
+            if(d == :present)
+              Time.now
+            else
+              d
+            end
+          end))
+      years  = t.in_years.floor
+      months = t.in_months.floor % 12
+
+      '( ' << 
+      if years > 1
+        "#{years} years"
+      elsif years == 1
+        '1 year'
+      else
+        ''
+      end << 
+      if months > 1
+        " #{months} months"
+      elsif months == 1
+        ' 1 month'
+      else
+       ''
+      end << ' ).'
+    end # _time_difference
+  end   # Document
+end     # Resume
